fa73dc69e7e2f2009941fbfb83135000ce43a446
PabloJuice <pawlo.dzhus@gmail.com>
2021-01-05 16:23:11 +0200

    merged

---
+++ src/main/java/com/softserve/itacademy/controller/TaskListServlet.java
@@ -0,0 +1,27 @@
+package com.softserve.itacademy.controller;
+
+import com.softserve.itacademy.repository.TaskRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(value = "/tasks-list")
+public class TaskListServlet extends HttpServlet {
+    private TaskRepository taskRepository;
+
+    @Override
+    public void init() throws ServletException {
+        taskRepository = TaskRepository.getTaskRepository();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/pages/tasks-list-page.jsp");
+        request.setAttribute("tasks",taskRepository.all());
+        requestDispatcher.forward(request,response);
+    }
+}
