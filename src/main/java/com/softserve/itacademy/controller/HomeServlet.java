0ce7df568d1d0077bcf784eb8b3f4f2590c72252
learningGuru <nazar.furdychka50@gmail.com>
2021-01-05 15:32:08 +0200

    working with home servlet

--- src/main/java/com/softserve/itacademy/controller/HomeServlet.java
+++ src/main/java/com/softserve/itacademy/controller/HomeServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(value = "/home")
+@WebServlet({"/", "/home"})
 public class HomeServlet extends HttpServlet{
 
     private TaskRepository taskRepository;
